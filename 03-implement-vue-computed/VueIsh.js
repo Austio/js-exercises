5500dd141d1d113b5650363cfc111c58e1de12d3
Austio <lonnieastory@gmail.com>
2017-08-03 22:53:45 -0500

    Use define property to get value of object

--- 03-implement-vue-computed/VueIsh.js
+++ 03-implement-vue-computed/VueIsh.js
@@ -1,6 +1,20 @@
+function defineProp(that, key, v) {
+  var val = v;
+
+  Object.defineProperty(that, key, {
+    enumerable: true,
+    get() {
+      return val;
+    },
+    set(newVal) {
+      val = newVal;
+    }
+  })
+}
+
 function VueIsh(options = {}) {
   for(let d in options.data) {
-    this[d] = options.data[d];
+    defineProp(this, d, options.data[d])
   }
   this.root = true;
 }
